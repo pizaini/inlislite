c95b76d7aace627abdc36ddec9494b64b7771630
Pizaini <zheiner.taurus@gmail.com>
2021-05-17 21:59:39 +0700

    sorted repo list

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -23,6 +23,26 @@ if(getenv('DB_PASSWORD') !== false){
     $db_password = getenv('DB_PASSWORD');
 }
 
+/**
+ * Mailer
+ */
+$mail_host = 'localhost';
+if(getenv('MAIL_HOST') !== false){
+    $mail_host = getenv('MAIL_HOST');
+}
+$mail_username = 'email@domain.com';
+if(getenv('MAIL_USERNAME') !== false){
+    $mail_username = getenv('MAIL_USERNAME');
+}
+$mail_password = 'password';
+if(getenv('MAIL_PASSWORD') !== false){
+    $mail_password = getenv('MAIL_PASSWORD');
+}
+$mail_port = '587';
+if(getenv('MAIL_PORT') !== false){
+    $mail_port = getenv('MAIL_PORT');
+}
+
 return [
     'components' => [
         'db' => [
@@ -38,7 +58,15 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => $mail_host,
+                'username' => $mail_username,
+                'password' => $mail_password,
+                'port' => $mail_port,
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
